1d1f884bba6e9549a189046c9add2c8dd3b83ec7
SR_team <sr-tream@yandex.ru>
2017-05-07 15:03:54 +0300

    Using prog name without suffix, for autoremove links and ini from history

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QFileInfo ths(argv[0]);
-    Interface w(ths.fileName());
+    Interface w(ths.fileName().remove(ths.suffix()));
     w.show();
 
     return a.exec();
